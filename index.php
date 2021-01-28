6b43e8555315943ca55bef952666489b180a5d4e
Debian <bitnami@ip-172-26-13-174.ec2.internal>
2021-01-28 11:42:08 +0000

    aaa

--- index.php
+++ index.php
@@ -1,17 +1,21 @@
 <?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
 /**
- * Tells WordPress to load the WordPress theme and output it.
+ * Laravel - A PHP Framework For Web Artisans
  *
- * @var bool
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
  */
-define( 'WP_USE_THEMES', true );
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+$uri = urldecode(
+    parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
+);
+
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the
+// built-in PHP web server. This provides a convenient way to test a Laravel
+// application without having installed a "real" web server software here.
+if ($uri !== '/' && file_exists(__DIR__.'/core'.$uri)) {
+    return false;
+}
+
+require_once __DIR__.'/core/server.php';
